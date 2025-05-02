ccfdc6ea8caf6a135994d7429b85468b94d0252f
nkkmkk <nikitmkk@gmail.com>
2025-05-02 14:38:51 +0300

    fix bug

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def cache(func: Callable) -> Callable:
     result_list = {}
 
     def inner(*args) -> Any:
-        if args not in result_list.keys():
+        if args not in result_list:
             result_list[args] = func(*args)
             print("Calculating new result")
         else:
